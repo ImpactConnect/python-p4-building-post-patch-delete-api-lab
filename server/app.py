88a581b8a69056aa3d45f2b084ff2759a3988673
Omotayo <kehindeomotayo11@gmail.com>
2023-11-11 10:50:20 +0100

    Updated

--- server/app.py
+++ server/app.py
@@ -45,5 +45,33 @@ def most_expensive_baked_good():
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
 
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def creates_baked_goods():
+    if request.method == "GET":
+        baked_goods_list = [bg.to_dict() for bg in BakedGood.query.all()]  # Query from the database
+        return jsonify(baked_goods_list)
+    elif request.method == 'POST':
+        new_name = request.form['name'],
+        new_price = request.form['price']
+        new_time = request.form['created_at']
+        new_update = request.form['updated_at']
+        
+        new_product = BakedGood(name=new_name, price=new_price, created_at=new_time, updated_at=new_update)
+        db.session.add(new_product)
+        db.session.commit()
+        return jsonify(new_product.to()), 201 
+    else:
+        return 'Method Not Allowed', 405 
+    
+@app.route('/baked_goods/<int:id>')
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get(id)
+    if baked_good:
+        db.session.delete(baked_good)
+        db.session.commit()
+        return jsonify({'message': f'Baked good with id {id} has been deleted successfully.'}), 200
+    else:
+        return jsonify({'message': 'Baked good not found.'}), 404
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
